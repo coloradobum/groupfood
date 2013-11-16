f53b0673a1598285e771a53ba442bb46754be102
Jeff Thomas <subskriptions@gmail.com>
2013-11-15 17:57:23 -0700

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupfood::Application.config.secret_key_base = '585c7e66dbc617451661cbb335172420e1ba569d3435403888b9548290ff0597157fd7d05d375b1b8fdd099f1a71e7848a0fc8fb291932a557cb4a02b75b9c53'
