2916c8404ebdf9cbeb8ad99786ac8ce6eb240126
Jeff Thomas <subskriptions@gmail.com>
2013-11-25 17:13:35 -0700

    fixed typo.

--- db/migrate/20131125234742_create_members.rb
+++ db/migrate/20131125234742_create_members.rb
@@ -2,7 +2,7 @@ class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
       t.string :name
-      t.int :trip_id
+      t.integer :trip_id
       t.float :expense
       t.text :description
 
